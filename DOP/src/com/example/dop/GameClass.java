cbb241210c3c8dfec835d38edb206ca46a9ff4d2
n8frog <n8frog@gmail.com>
2014-06-04 20:49:44 -0400

    Game Class
    Game class written and stubbed out with possible members and member
    functions

---
+++ DOP/src/com/example/dop/GameClass.java
@@ -0,0 +1,62 @@
+package com.example.dop;
+
+public class GameClass extends MainActivity {
+
+	Enemy enemies []; // pool of enemy objects
+	Obstacle obstacles []; // pool of obstacle objects
+	Transformer transformers []; // pool of transformer objects
+	Cloud clouds []; // pool of cloud objects
+	PowerLine powerlines []; // pool of power line objects
+	Turd turds []; // pool of bird turd objects
+	Player player; // the player object
+	Cat cat;	// the cat object
+	Camera camera; // the camera view object
+	
+	public Enemy getEnemy(){ // returns an enemy object from the pool
+		
+	}
+	
+	public Obstacle getObstacle(){ // returns an obstacle object from the pool
+		
+	}
+	
+	public Transformer getTransformer(){ // returns a transformer object from the pool
+		
+	}
+	
+	public Cloud getCloud(){ // returns a cloud object from the pool
+		
+	}
+	
+	public PowerLine getPowerLine(){ // returns a power line object from the pool
+		
+	}
+	
+	public Turd getTurd(){ // returns a bird turd object from the pool
+		
+	}
+	
+	public void recycleEnemy(Enemy){ // puts the passed in enemy object back into the pool
+		
+	}
+	
+	public void recycleObstacle(Obstacle){ // puts the passed in Obstacle object back into the pool
+		
+	}
+	
+	public void recycleTransformer(Transformer){ // puts the passed in Transformer object back into the pool
+		
+	}
+	
+	public void recycleCloud(Cloud){ // puts the passed in Cloud object back into the pool
+		
+	}
+	
+	public void recyclePowerLine(PowerLine){ // puts the passed in PowerLine object back into the pool
+		
+	}
+	
+	public void recycleTurd(Turd){ // puts the passed in Turd object back into the pool
+		
+	}
+}
