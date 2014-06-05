9ac0d5556370dc77217a5f5f2cd6957dc2797b9e
n8frog <n8frog@gmail.com>
2014-06-05 17:01:45 -0400

    Changed GameScreen class

--- DOP/src/com/example/dop/GameClass.java
+++ DOP/src/com/example/dop/GameClass.java
@@ -1,16 +1,34 @@
 package com.example.dop;
+import java.util.*;
+import com.badlogic.androidgames.framework.*;
+import com.badlogic.androidgames.framework.Input.*;
+import com.badlogic.androidgames.framework.impl.*;
 
-public class GameClass extends MainActivity {
+public class GameScreen extends Screen {
 
-	Enemy enemies []; // pool of enemy objects
-	Obstacle obstacles []; // pool of obstacle objects
-	Transformer transformers []; // pool of transformer objects
-	Cloud clouds []; // pool of cloud objects
-	PowerLine powerlines []; // pool of power line objects
-	Turd turds []; // pool of bird turd objects
+	Enemy enemies []; // pool of enemy objects - size 6 objects
+	Obstacle obstacles []; // pool of obstacle objects - size 5 objects
+	Transformer transformers []; // pool of transformer objects - size 3 objects
+	ForegroundCloud fgclouds []; // pool of cloud objects - size 4 objects
+	BackgroundCloud bgclouds []; // pool of cloud objects - size 4 objects
+	PowerLine powerlines []; // pool of power line objects - size 3 objects
+	Turd turds []; // pool of bird turd objects - size 10 objects?
 	Player player; // the player object
 	Cat cat;	// the cat object
 	Camera camera; // the camera view object
+	
+	enum GameState {
+		Ready,
+		Running,
+		Paused,
+		GameOver
+	}
+	
+	GameState state = GameState.Ready;
+	
+	public GameScreen(Game game) {
+		super(game);
+	}
 	
 	public Enemy getEnemy(){ // returns an enemy object from the pool
 		
@@ -24,7 +42,11 @@ public class GameClass extends MainActivity {
 		
 	}
 	
-	public Cloud getCloud(){ // returns a cloud object from the pool
+	public ForegroundCloud getFgCloud(){ // returns a cloud object from the pool
+		
+	}
+	
+	public BackgroundCloud getBgCloud(){ // returns a cloud object from the pool
 		
 	}
 	
@@ -48,7 +70,11 @@ public class GameClass extends MainActivity {
 		
 	}
 	
-	public void recycleCloud(Cloud){ // puts the passed in Cloud object back into the pool
+	public void recycleFgCloud(ForegroundCloud){ // puts the passed in Cloud object back into the pool
+		
+	}
+	
+	public void recycleBgCloud(BackgroundCloud){ // puts the passed in Cloud object back into the pool
 		
 	}
 	
@@ -57,6 +83,49 @@ public class GameClass extends MainActivity {
 	}
 	
 	public void recycleTurd(Turd){ // puts the passed in Turd object back into the pool
+		
+	}
+	
+	public void update(float deltaTime) { // updates game logic and object positions, uses deltaTime
+		List<TouchEvent>touchEvents = game.getInput().getTouchEvents();
+	}
+	
+	private void updateReady(List <TouchEvent> touchEvents) {
+		if(touchEvents.size() > 0)
+				state = GameState.Running;
+	}
+	
+	private void updateRunning(List <TouchEvent> touchEvents, float deltaTime) {
+		int len = touchEvents.size();
+		for(int i = 0; i < len; ++i){
+			TouchEvent event = touchEvents.get(i);
+			if(event.type == TouchEvent.TOUCH_UP) {
+				// Do something
+			}
+		}
+	}
+	
+	private void updatePaused(List <TouchEvent> touchEvents) {
+		int len = touchEvents.size();
+		for(int i = 0; i < len; ++i){
+			TouchEvent event = touchEvents.get(i);
+			if(event.type == TouchEvent.TOUCH_UP) {
+				// Do something
+			}
+		}
+	}
+	
+	private void updateGameOver(List <TouchEvent> touchEvents) {
+		int len = touchEvents.size();
+		for(int i = 0; i < len; ++i){
+			TouchEvent event = touchEvents.get(i);
+			if(event.type == TouchEvent.TOUCH_UP) {
+				// Do something
+			}
+		}
+	}
+	
+	public void present(float deltaTime) { // renders game objects, uses deltaTime
 		
 	}
 }
